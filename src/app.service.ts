2f3b80286a685f697e19793994726ec0e3b47daa
kuku20 <lluu3790@gmail.com>
2024-01-22 15:32:55 -0600

    ready to deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
+// cors-proxy.service.ts
 import { Injectable } from '@nestjs/common';
+import * as corsProxy from 'cors-anywhere';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  startProxyServer() {
+    const host = process.env.HOST || '0.0.0.0';
+    const port = process.env.PORT || 3000;
+
+    corsProxy
+      .createServer({
+        originWhitelist: [], // Allow all origins
+        requireHeader: ['origin', 'x-requested-with'],
+        removeHeaders: ['cookie', 'cookie2'],
+      })
+      .listen(port, host, () => {
+        console.log(`Running CORS Anywhere on ${host}:${port}`);
+      });
   }
 }
